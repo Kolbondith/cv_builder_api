66be815e85628cffd59a6ed24c341a7db7b05232
Kolbondith <kolbondith@gmail.com>
2026-01-20 16:29:47 +0700

    enhance ai

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -18,3 +18,67 @@ export const createResume = async (req, res) => {
         return res.status(400).json({ message: error.message })
     }
 }
+
+//Delete: for deleting a resume
+
+export const deleteResume = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const { resumeId } = req.params;
+
+        // create new resume 
+        await Resume.findOneAndDelete({ userId, _id: resumeId })
+        // return success message 
+        return res.status(200).sjon({ message: 'resume delete successfully' })
+    } catch (error) {
+        return res.status(400).json({ message: error.message })
+    }
+}
+
+
+// get user resume by id 
+//Get: /api/resumes/get 
+
+export const getResumeById = async (req, res) => {
+    try {
+        const userId = req.userId;
+        const { resumeId } = req.params;
+
+        const resume = await Resume.findOne({ userId, _id: resumeId });
+
+        if (!resume) {
+            return res.status(404).json({ message: "Resume not found" })
+        }
+
+        resume.__v = undefined;
+        resume.createdAt = undefined;
+        resume.updatedAt = undefined;
+
+        return res.status(200).json({ resume })
+
+
+    } catch {
+        return res.status(400).json({ message: error.message })
+    }
+
+}
+
+// get resume by id public
+//Get : /api/resumes/public 
+
+export const getPublicResumeById = async (req, res) => {
+    try {
+        const { resumeId } = req.params;
+        const resume = await Resume.findOne({ public: true, _id: resumeId });
+
+        if (!resume) {
+            return res.status(404).json({ message: "status not found" })
+        }
+
+        return res.status(200).json({ resume })
+    } catch (error) {
+        return res.status(400).json({ message: error.message })
+    }
+}
+
+// Patch: /api/resume/edit
